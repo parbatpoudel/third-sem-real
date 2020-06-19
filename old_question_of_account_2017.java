09080d16559331a12397e747f1a5c0f33a1e0d6e
parbatpoudel <67102224+parbatpoudel@users.noreply.github.com>
2020-06-19 22:01:11 +0545

    Create old_question_of_account_2017.java

---
+++ old_question_of_account_2017.java
@@ -0,0 +1,97 @@
+package com.example.last.bbathirdsemester;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class old_question_of_account_2017 extends AppCompatActivity {
+
+    private TextView old_question_text_of_account_2017;
+    private PDFView old_question_of_account_2017;
+
+    private ProgressDialog dialog;
+
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference mref=database.getReference("old_question_of_account_2017");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_old_question_of_account_2017);
+
+        getSupportActionBar().setTitle("Old Question 2017");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        old_question_of_account_2017 = (PDFView) findViewById(R.id.old_question_of_account_2017);
+        old_question_text_of_account_2017 = (TextView) findViewById(R.id.old_question_text_of_account_2017);
+
+        dialog = new ProgressDialog(old_question_of_account_2017.this);
+        dialog.setTitle("Loading...");
+        dialog.setMessage("need internet connection");
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+
+        mref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String value=dataSnapshot.getValue(String.class);
+                old_question_text_of_account_2017.setText(value);
+                String Url=old_question_text_of_account_2017.getText().toString();
+                new RetrievePdfStream().execute(Url);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+
+            }
+        });}
+
+    class RetrievePdfStream extends AsyncTask<String,Void, InputStream> {
+
+        @Override
+        protected InputStream doInBackground(String... strings) {
+            InputStream inputStream = null;
+            try{
+                URL url = new  URL (strings[0]);
+                HttpsURLConnection urlConnection = (HttpsURLConnection)url.openConnection();
+                if (urlConnection.getResponseCode()==200)
+                {
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                }
+            }catch (IOException e){
+                return null;
+            }
+            return inputStream;
+
+        }
+
+        @Override
+        protected void onPostExecute(InputStream inputStream) {
+            old_question_of_account_2017.fromStream(inputStream).load();
+
+            if (dialog.isShowing()){
+                dialog.dismiss();
+            }
+        }
+    }
+}
