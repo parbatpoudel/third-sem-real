5a51c30cb1c787da62f847b1811405a74a41cb5b
parbatpoudel <67102224+parbatpoudel@users.noreply.github.com>
2020-06-19 22:13:14 +0545

    Create Syllabus of financial accounting.java

---
+++ Syllabus of financial accounting.java
@@ -0,0 +1,99 @@
+package com.example.last.bbathirdsemester;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class Syllabus_of_financial_accounting extends AppCompatActivity {
+
+    private TextView syllabus_text_of_financial_accounting;
+    private PDFView syllabus_of_financial_accounting;
+
+
+    private ProgressDialog dialog;
+
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference mref=database.getReference("syllabus_of_financial_accounting");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_syllabus_of_financial_accounting);
+
+        getSupportActionBar().setTitle("Syllabus of Financial Accounting");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        syllabus_of_financial_accounting = (PDFView) findViewById(R.id.syllabus_of_financial_accounting);
+        syllabus_text_of_financial_accounting = (TextView) findViewById(R.id.syllabus_text_of_financial_accounting);
+
+        dialog = new ProgressDialog(Syllabus_of_financial_accounting.this);
+        dialog.setTitle("Loading...");
+        dialog.setMessage("need internet connection");
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+
+        mref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String value=dataSnapshot.getValue(String.class);
+                syllabus_text_of_financial_accounting.setText(value);
+                String Url=syllabus_text_of_financial_accounting.getText().toString();
+                new RetrievePdfStream().execute(Url);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+
+            }
+        });}
+
+    class RetrievePdfStream extends AsyncTask<String,Void, InputStream> {
+
+        @Override
+        protected InputStream doInBackground(String... strings) {
+            InputStream inputStream = null;
+            try{
+                URL url = new  URL (strings[0]);
+                HttpsURLConnection urlConnection = (HttpsURLConnection)url.openConnection();
+                if (urlConnection.getResponseCode()==200)
+                {
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                }
+            }catch (IOException e){
+                return null;
+            }
+            return inputStream;
+
+        }
+
+        @Override
+        protected void onPostExecute(InputStream inputStream) {
+            syllabus_of_financial_accounting.fromStream(inputStream).load();
+
+            if (dialog.isShowing()){
+                dialog.dismiss();
+            }
+        }
+    }
+}
